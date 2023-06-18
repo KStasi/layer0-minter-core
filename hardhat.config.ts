05ef36264223d069c22f69e5e3d0ffbd2e3497f4
KStasi <nastasiakondaurova@gmail.com>
2023-06-18 17:23:47 +0300

    Init version

---
+++ hardhat.config.ts
@@ -0,0 +1,18 @@
+import { HardhatUserConfig } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+import "@nomiclabs/hardhat-ethers";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
+
+export default config;
